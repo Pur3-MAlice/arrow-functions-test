bc20ec5eea617006e474df09dd01c3ff79e7351e
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-14 16:00:19 +0000

    uses rest and spread operator

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -1,2 +1,20 @@
 const sumAll = (a, b, c) => a + b + c;
-console.log(sumAll(1,2,3));
+let sum = sumAll (1, 2, 3);
+
+console.log("Sum:", sum);
+
+let sum2 = sumAll(1, 2 ,3, 4, 5, 6);
+
+console.log("Sum2:", sum);
+
+
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum +=i;
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2 ,3, 4, 5, 6);
+console.log("Sum3:", sum3)
