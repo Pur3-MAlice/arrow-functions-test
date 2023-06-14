cc3465bcd1b6d8eff328f0ada30b615b13a97a74
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-14 14:36:57 +0000

    adds single line arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -14,12 +14,14 @@
 const addTwoNumbers = (a, b) => {
     return a + b;
 }
-console.log(addTwoNumbers(2, 5))
-console.log(addTwoNumbers(4, 3))
-console.log(addTwoNumbers(22, 7))
+console.log(addTwoNumbers(2, 5));
+
 
 
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbers2 = (a, b) => a + b;
+console.log(addTwoNumbers2(22, 7));
 
 // Implicit Returns
 
