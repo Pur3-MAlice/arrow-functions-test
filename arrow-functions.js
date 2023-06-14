6f1c24cd6ac907733f2fbd961b818c6e42a0a025
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-14 14:31:23 +0000

    adds arrow function equiv to js for testing

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,13 +5,19 @@
 
 // Vanilla JavaScript Function
 
-function addTwoNumbers(a, b) {
-    return a + b;
-}
-
-console.log(addTwoNumbers(2, 5))
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// console.log(addTwoNumbers(2, 5))
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+console.log(addTwoNumbers(2, 5))
+console.log(addTwoNumbers(4, 3))
+console.log(addTwoNumbers(22, 7))
+
 
 // Single Line Arrow Function With Parameters
 
