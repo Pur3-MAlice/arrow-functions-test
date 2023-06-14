27525f28bdebe9a3eb567e32badf4f3cebc8fa47
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-14 15:51:39 +0000

    testing spread operator

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -1,5 +1,11 @@
 // no spread operator
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = arr1;
 
+arr2.push(6);
+
+console.log("second: ", arr2);
+console.log("first: ", arr1);
 // copying and array
 
 // copying and object 
